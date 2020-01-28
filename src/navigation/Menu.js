9ecf39d90d011ee8963c7d9969353bf7dec1b6dd
Daniel <danielbangwhs@gmail.com>
2020-01-28 12:34:58 -0800

    Adds a skeleton for the checkout page

--- src/navigation/Menu.js
+++ src/navigation/Menu.js
@@ -26,6 +26,7 @@ const Menu = () => {
         <IonList>
           <NavItem route={routes.HomePage} label="Home" />
           <NavItem route={routes.SignUpPage} label="Sign Up" />
+          <NavItem route={routes.CheckoutPage} label="Checkout" />
         </IonList>
       </IonContent>
     </IonMenu>
